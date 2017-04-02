14abf0f1e6c12414fae7db067015e8be1c10787f
Tan Bui at home <tan.bui.home@gmail.com>
2017-04-02 09:43:51 +0300

    API to append categories of a post

--- wp-content/plugins/restful-api/FCMRestfulController.php
+++ wp-content/plugins/restful-api/FCMRestfulController.php
@@ -21,7 +21,39 @@ class FCMRestfulController extends WP_REST_Controller {
                 'callback'        => array( $this, 'create_item' ),
                 'permission_callback' => [$this, 'create_item_permissions_check']
             ),
+            array(
+                'methods'         => WP_REST_Server::EDITABLE,
+                'callback'        => array( $this, 'update_item' ),
+                'permission_callback' => [$this, 'create_item_permissions_check']
+            )
         ) );
+    }
+
+    /**
+     * Append a category to a post's categories
+     *
+     * @param WP_REST_Request $request
+     * @return WP_REST_Response | WP_Error
+     *
+        PUT /FCMadminWP/wp-json/fcm/v1/device HTTP/1.1
+        Host: localhost
+        Content-Type: application/x-www-form-urlencoded
+        Authorization: myownsecretkey
+        Cache-Control: no-cache
+        Postman-Token: 82a966be-73a3-1fff-dbbb-5577046799e1
+
+        device=eVCsmuOnpzM%3AAPA91bHnJ6n9w5GtsKdAm2eWC-vrOUOtAjN_IhFhcTM4bCR87Y98kJELcqo_TmeWjDoAok-UaQgt8qsgssrsB_tFKyqmdBbgpAKGV4-t4wz1xKzRUU-dOazUdLdXQNGbX9JS-jnA8SIA&cat_name=exchange+student
+     *
+     */
+    public function update_item($request)
+    {
+        $post_id = get_page_by_title($request->get_param('device'), OBJECT, 'post')->ID;
+        $result = wp_set_post_categories($post_id, get_cat_ID($request->get_param('cat_name')), true);
+        if(is_array($result)) {
+            return new WP_REST_Response( ['Success'] , 200 );
+        } else {
+            return new WP_Error('Failed', 'Failed');
+        }
     }
 
     /**
@@ -29,6 +61,13 @@ class FCMRestfulController extends WP_REST_Controller {
      *
      * @param WP_REST_Request $request Full data about the request.
      * @return WP_REST_Response | WP_Error
+     *
+     *  POST /FCMadminWP/wp-json/fcm/v1/device HTTP/1.1
+        Host: localhost
+        Content-Type: application/x-www-form-urlencoded
+        Authorization: myownsecretkey
+
+        device=eVCsmuOnpzM%3AAPA91bHnJ6n9w5GtsKdAm2eWC-vrOUOtAjN_IhFhcTM4bCR87Y98kJELcqo_TmeWjDoAok-UaQgt8qsgssrsB_tFKyqmdBbgpAKGV4-t4wz1xKzRUU-dOazUdLdXQNGbX9JS-jnA8SIA
      */
     public function create_item( $request ) {
         $post_id = wp_insert_post([
